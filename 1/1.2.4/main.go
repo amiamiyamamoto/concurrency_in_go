ce031e270b426c4c15ba93bd1706345ec1ea2ecb
amiamiyamamoto <amiyamamoto11@gmail.com>
2023-01-15 21:44:49 +0900

    デッドロック

---
+++ 1/1.2.4/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type value struct {
+	mu    sync.Mutex
+	value int
+}
+
+func main() {
+	var wg sync.WaitGroup
+	printSum := func(v1, v2 *value) {
+		v1.mu.Lock()
+		defer v1.mu.Unlock()
+
+		fmt.Printf("sum=%v\n", v1.value+v2.value)
+	}
+
+	var a, b, value
+	wg.Add(2)
+	go printSum(&a, &b)
+	go printSum(&b, &a)
+	wg.Wait()
+}
